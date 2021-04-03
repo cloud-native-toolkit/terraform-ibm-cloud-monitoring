1eb05bd3f041cf38873a8daea51a95974eb2a8be
Sean Sundberg <seansund@us.ibm.com>
2021-04-02 23:22:40 -0500

    Adds guid and key_name to outputs
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,21 @@
 output "id" {
   value       = data.ibm_resource_instance.sysdig_instance.id
-  description = "The id of the provisioned LogDNA instance."
+  description = "The id of the provisioned Sysdig instance."
 }
 
 output "name" {
   value       = local.name
   depends_on  = [ibm_resource_instance.sysdig_instance]
-  description = "The name of the provisioned LogDNA instance."
+  description = "The name of the provisioned Sysdig instance."
+}
+
+output "guid" {
+  value       = data.ibm_resource_instance.sysdig_instance.guid
+  description = "The id of the provisioned Sysdig instance."
+}
+
+output "key_name" {
+  value       = local.name
+  depends_on  = [ibm_resource_key.sysdig_instance_key]
+  description = "The name of the key provisioned for the Sysdig instance."
 }

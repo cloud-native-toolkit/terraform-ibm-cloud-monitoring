f0735907acfb26d5e406fc3a1274b7a04abb3fad
Sean Sundberg <seansund@us.ibm.com>
2021-04-16 02:36:54 -0500

    Pass ibmcloud_api_key explicitly
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-sysdig.tf
+++ test/stages/stage2-sysdig.tf
@@ -2,6 +2,7 @@ module "dev_sysdig" {
   source = "./module"
 
   resource_group_name      = module.resource_group.name
+  ibmcloud_api_key         = var.ibmcloud_api_key
   region                   = var.region
   provision                = true
   name_prefix              = var.name_prefix
